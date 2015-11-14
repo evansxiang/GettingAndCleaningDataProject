54f7d0868ae359207fd394bcafd58280a7423944
evansxiang <evans.xiang@gmail.com>
2015-11-14 10:55:19 -0500

    Updating README and run scripts

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 run_analysis.R is the main script that runs everything.
 
 * It first downloads the file to the current directory and named ProjectData.zip
+* The mergeData() function merges all the data and puts them in the MergedData directory, the merged.csv contains everything combined together
--- run_analysis.R
+++ run_analysis.R
@@ -1,2 +1,8 @@
-#first download the raw zip file
+#first download and unzip the raw zip file
 download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip", "./ProjectData.zip")
+unzip("./ProjectData.zip")
+
+#merge data
+library(data.table)
+source("./mergeData.R")
+mergeData("UCI Har Dataset")
